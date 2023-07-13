48a6fe7cc59971acb7f6f82b7573a07e98355b60
Nadia Matveeva <nadiamat@uw.edu>
2023-07-13 15:58:19 -0700

    sizing
    fixed spatial component size
    checked the class works with the full sized components

--- .ipynb_checkpoints/wfData_fullsize-checkpoint.py
+++ .ipynb_checkpoints/wfData_fullsize-checkpoint.py
@@ -7,6 +7,8 @@ import scipy
 class wfData:
     '''
     creates all necessary items for working with widefield imaging data
+    defines functions for creating various types of videos
+    defines functions for creating various types of activity graphs
     '''
     def __init__(self, pathSubject, pathStim):
         '''
@@ -16,13 +18,14 @@ class wfData:
         self.timeFile = serverPath / 'cameraFrameTimes.npy'
         self.frameTimes = np.squeeze(np.load(self.timeFile))[::2]
         self.svdTemp = np.load(serverPath / 'corr/svdTemporalComponents_corr.npy')
-        self.svdSpat = np.load(serverPath / 'blue/svdSpatialComponents.npy')
+        self.svdSpatRaw = np.load(serverPath / 'blue/svdSpatialComponents.npy')
+        self.svdSpat500 = self.svdSpatRaw[:,:,:500]
         self.meanImage = np.load(serverPath / 'blue/meanImage.npy')
         self.stimOnTimes = np.squeeze(np.load(pathStim / 'linTestOnTimes.npy'))
         self.stimContrasts = np.squeeze(np.load(pathStim / 'linTestContrasts.npy'))
         self.stimDurations = np.squeeze(np.load(pathStim / 'linTestDurations.npy'))
         self.getTempComp = scipy.interpolate.interp1d(self.frameTimes, self.svdTemp, axis=0)
-        self.spatial = (self.svdSpat).reshape(560*560, -1)
+        self.spatial = (self.svdSpat500).reshape(560*560, -1)
         self.contrasts = np.unique(self.stimContrasts)
         self.durations = np.unique(self.stimDurations)
     def getContrasts():
@@ -30,6 +33,12 @@ class wfData:
     def getDurations():
         return self.durations
     def trial_timeFunc(self, start, end, step, oneTime=False, loc=-1):
+        '''
+        creates trial timings for a specified start before time, an end after the time, with a 'step' count of
+        time points in between.
+        oneTime deliniates if we are creating trial times around one trial (True) or around all trials (False)
+        loc is also only used if we are creating trial times around one stimulus (oneTime must be true)
+        '''
         self.oneTime = oneTime
         if oneTime==True:
             startTime = self.stimOnTimes[loc] - start
@@ -42,23 +51,35 @@ class wfData:
         return self.trial_time
     def getTempCompFunc(self, trial_time_input):
         '''
-        uses the interpolation function to return values
+        defines the interpolation function used to return trial activity
         '''
         return self.getTempComp(trial_time_input)
     def trial_activityFunc(self, trial_time_input):
+        '''
+        uses getTempCompFunc to create trial activity for the desired trial times
+        '''
         self.trial_activity = self.getTempCompFunc(trial_time_input)
         self.trial_activity = np.array(self.trial_activity)
         return self.trial_activity
     def createVideo(self, trial_activity):
+        '''
+        checks if we want to make a video for a single timepoint
+        oneTime is created prior in trial_timeFunc if we are creating a video around just one trial 
+        '''
         if self.oneTime==False:
             trial_activity_re = np.mean(trial_activity, axis=0)
         else:
             trial_activity_re = trial_activity
-        spatial = (self.svdSpat).reshape(560*560, -1)
-        self.video = spatial @ trial_activity_re.T
+        self.video = self.spatial @ trial_activity_re.T
         self.video = (self.video).reshape(560,560,-1)
         return self.video
     def plotBrain(self, rows=1, cols=1, avg=False, figsizeCol=2, figsizeRow=2):
+        '''
+        creates an image of the brain's activity
+        can create any number of images or any sort of grid size
+        avg allows us to create a picture of the average activity throughout all wanted trials 
+                                                    (deliniated by the size of trial_activity)
+        '''
         if avg==True:
             f = plt.figure(figsize=(cols*figsizeCol, rows*figsizeRow))
             avg_trial_activity = np.mean(self.trial_activity, axis=1)
@@ -76,6 +97,9 @@ class wfData:
 
             f.tight_layout()
     def createContrastVideos(self, start, stop, step):
+        '''
+        creates videos of pixel values across all contrasts for desired trials (deliniated by trial_time)
+        '''
         videos=[]
         for contrast in self.contrasts:
             theseIndexes = np.squeeze(np.argwhere(self.stimContrasts == contrast))
@@ -93,6 +117,9 @@ class wfData:
         videos = np.array(videos)
         return videos
     def createDurationVideos(self, start, stop, step):
+        '''
+        creates videos of pixel values across all durations for desired trials
+        '''
         videos=[]
         for dur in self.durations:
             theseIndexes = np.squeeze(np.argwhere(self.stimDurations==dur))
@@ -106,8 +133,8 @@ class wfData:
         videos=np.array(videos)
         return videos
     def createConDurVid(self, start, stop, step, durCount):
-        ''' 
-        broken
+        '''
+        creates videos of pixel values across all contrasts for each separate duration
         '''
         videos = [[] for x in range(durCount)]
         for count,duration in enumerate(self.durations[:durCount]):
@@ -128,6 +155,9 @@ class wfData:
         videos = np.array(videos)
         return videos
     def corrections(self, frames):
+        '''
+        does a df/f calculation for pixel values in a video
+        '''
         videoCorr = np.mean(frames, axis=(0, 1))
         minActivity = min(videoCorr)
         if minActivity < 0:

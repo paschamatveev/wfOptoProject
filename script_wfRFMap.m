dbcb3a3ed52acbe15618b56eb49a24d2e87b7f37
Nadia Matveeva <nadiamat@uw.edu>
2024-08-12 12:24:47 -0700

    compare step and oscillation

---
+++ script_wfRFMap.m
@@ -0,0 +1,35 @@
+githubDir = 'C:\Users\anna\Repositories';
+
+addpath(genpath(fullfile(githubDir, 'spikes')))
+addpath(genpath(fullfile(githubDir, 'npy-matlab')))
+addpath(genpath(fullfile(githubDir, 'Pipelines')))
+
+
+%%
+
+mn = 'ZYE_0077';
+td = '2024-07-22';
+en = 1;
+serverRoot = expPath(mn, td, en);
+%% exp numbers: at least the camera folder and the stimulus folder
+success = matchBlocks2Timeline(mn,td,[1 4], []);
+
+%% receptive fields 
+
+rfExpNum = 4;
+block = getBlockFile(mn, td, rfExpNum);
+expRoot = expPath(mn, td, rfExpNum);
+
+pdTimes = readNPY(fullfile(expRoot, 'photodiodeFlips.timestamps.npy'));
+
+[stimTimeInds, stimPositions, stimArray, xPos, yPos] = computeSparseNoiseSignals(block);
+
+stimOnTimes = pdTimes(stimTimeInds{1});
+stimOffTimes = pdTimes(stimTimeInds{2});
+stimPos = stimPositions{1}; 
+
+%% save rf map stimulus info
+
+writeNPY(stimOnTimes, fullfile(expRoot, 'rfOnTimes.npy'))
+writeNPY(stimOffTimes, fullfile(expRoot, 'rfOffTimes.npy'))
+writeNPY(stimPos, fullfile(expRoot, 'rfPositions.npy'))

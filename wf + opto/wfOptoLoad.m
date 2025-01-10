b6c2f5825299dad63fa90d139500a786db69036c
Nadia Matveeva <nadiamat@uw.edu>
2025-01-09 17:09:06 -0800

    log stuff

--- wf + opto/wfOptoLoad.m
+++ wf + opto/wfOptoLoad.m
@@ -1,6 +1,6 @@
 %% Script to analyze a new widefield+opto recording
 
-githubDir = 'C:\Users\nadia\Documents\GitHb\wf gui';
+githubDir = ['C:\Users\nadia\Documents\GitHb\wf gui'];
 % githubDir= 'C:\GitHub\wf gui';
 
 %% get galvos, laserpowers, etc as before - assuming step function, and putting all in #1 folder
@@ -11,12 +11,12 @@ addpath(genpath(fullfile(githubDir, 'npy-matlab'))) % kwikteam/npy-matlab
 % addpath(genpath(fullfile(githubDir, 'wheelAnalysis'))) % cortex-lab/wheelAnalysis
 
 mn = 'AL_0033'; 
-td = '2024-12-11';
+td = '2024-12-17';
 ca_en = 1; % widefield
 
 serverRoot = expPath(mn, td, ca_en);
 
-expHz = [false true true true]; %
+expHz = [false false]; %
 
 
 %% process - the original way, not multiple exps, and WITHOUT 40hz
